958aa5ede7e56877428da3166a99ed069fa5421c
Michael Noah <michaelnoah.ea@gmail.com>
2020-09-29 18:56:15 +0300

    Updates

---
+++ precache-manifest.b6f9d628e5d8227bcbff8e5c7bdd471f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7d58954f818646c1fb3d5c945f2d847",
+    "url": "/Michael-Noah/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ec67f4e40d7cfc51e985",
+    "url": "/Michael-Noah/monsters-rolodex/static/css/main.5c9301e8.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/Michael-Noah/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Michael-Noah/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec67f4e40d7cfc51e985",
+    "url": "/Michael-Noah/monsters-rolodex/static/js/main.7db7ccfd.chunk.js"
+  },
+  {
+    "revision": "455ebdebdc0d9d47bddd",
+    "url": "/Michael-Noah/monsters-rolodex/static/js/runtime-main.82dfcea0.js"
+  }
+]);
